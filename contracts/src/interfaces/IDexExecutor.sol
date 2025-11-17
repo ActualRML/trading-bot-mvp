0b4f79adb5b406196622125bc3498a7f1b361b19
ActualRML <rmlilipaly@gmail.com>
2025-11-17 13:27:01 +0700

    Clean contracts folder and setup new Foundry structure

--- contracts/src/interfaces/IDexExecutor.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
-
-
-interface IDexExecutor {
-    function executeSwap(address tokenIn, address tokenOut, uint256 amountIn) external;
-}
--- contracts/src/interfaces/ISessionRegistry.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.30;
-
-
-interface ISessionRegistry {
-    function startSession() external;
-    function endSession() external;
-    function isActive(address user) external view returns (bool);
-}
